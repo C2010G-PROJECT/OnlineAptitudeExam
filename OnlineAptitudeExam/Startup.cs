c6257ccb26d474d1a78be2dc0cbc58a71572843d
SON.NC <son15052002@gmail.com>
2022-09-16 17:55:12 +0700

    Add project files.

---
+++ OnlineAptitudeExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineAptitudeExam.Startup))]
+namespace OnlineAptitudeExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
