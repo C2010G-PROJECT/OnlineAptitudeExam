de77cde34f3930210abae467c37db0694d132295
NamTranMaster <trandinhnamnd0102@gmail.com>
2022-09-21 19:52:51 +0700

    giao dien profile, edit profile, about _ miss update profile

--- OnlineAptitudeExam/Controllers/HomeController.cs
+++ OnlineAptitudeExam/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using OnlineAptitudeExam.Models;
 
 namespace OnlineAptitudeExam.Controllers
 {
     public class HomeController : Controller
     {
+        private OnlineAptitudeExamEntities dbEntities = new OnlineAptitudeExamEntities();
         public ActionResult Index()
         {
             return View();
@@ -26,9 +34,69 @@ namespace OnlineAptitudeExam.Controllers
         public ActionResult Profile()
         {
             ViewBag.Message = "Your contact page.";
+            Account user = Session["UserInfo"] as Account;
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            else
+            {
+                ViewBag.accountLogin = user;
+            }
+
+            ViewBag.accountLogin = user;
 
             return View();
         }
 
+        public ActionResult EditProfile()
+        {
+            Account user = Session["UserInfo"] as Account;
+
+
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            else
+            {
+                ViewBag.accountLogin = user;
+            }
+
+            ViewBag.accountEdit = user;
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult EditProfile(FormCollection key)
+        {
+            Account modelEdit = Session["UserInfo"] as Account;
+
+            if (modelEdit != null) {
+                string sAvatar = key["NameAvatar"];
+                string sFullName = key["fullname"];
+                string sUseName = key["username"];
+                string sAge = key["age"];
+                string sAddress = key["address"];
+                string sDescriptions = key["descriptions"];
+
+                modelEdit.avatar = sAvatar;
+                modelEdit.fullname = sFullName;
+                modelEdit.username = sUseName;
+                if (sAge != null) {
+                    modelEdit.age = Int32.Parse(sAge);
+                }
+                modelEdit.address = sAddress;
+                modelEdit.descriptions = sDescriptions;
+                
+                UpdateModel(modelEdit);
+                dbEntities.SaveChanges();
+
+            }
+
+            return View();
+        }
     }
 }
+
