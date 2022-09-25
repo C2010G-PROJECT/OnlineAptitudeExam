2af5cd14688c61ca9a2afce297c7848cb89be43a
SON.NC <son15052002@gmail.com>
2022-09-26 02:34:44 +0700

    setup account controller

---
+++ OnlineAptitudeExam/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using OnlineAptitudeExam.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineAptitudeExam.Areas.Admin.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Admin/Accounts
+        [Authentication(true)]
+        public ActionResult Index(bool isAjax = false)
+        {
+            ViewBag.isAjax = isAjax;
+            return View();
+        }
+    }
+}
