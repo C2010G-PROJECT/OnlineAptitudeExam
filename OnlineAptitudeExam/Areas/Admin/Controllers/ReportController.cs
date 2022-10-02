803f49338fe2775455f484553073b65dac075db8
NamTranMaster <trandinhnamnd0102@gmail.com>
2022-10-02 15:29:45 +0700

    report admin _todo_

--- OnlineAptitudeExam/Areas/Admin/Controllers/ReportController.cs
+++ OnlineAptitudeExam/Areas/Admin/Controllers/ReportController.cs
@@ -1,15 +1,48 @@
-﻿using OnlineAptitudeExam.Utils;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
+using OnlineAptitudeExam.Models;
+using OnlineAptitudeExam.Utils;
 
 namespace OnlineAptitudeExam.Areas.Admin.Controllers
 {
     public class ReportController : Controller
     {
+
+        private readonly OnlineAptitudeExamEntities db = new OnlineAptitudeExamEntities();
         // GET: Admin/Report
         [Authentication(true)]
         public ActionResult Index(bool isAjax = false)
         {
             ViewBag.isAjax = isAjax;
+            return View();
+        }
+
+        // GET: Admin/Tests/GetData
+        [Authentication(true)]
+        [ValidateInput(false)]
+        public ActionResult GetDataReport(string nameExam, string dateStart, string dateEnd, int? page)
+        {
+
+            /*SELECT Test.name, COUNT(Exam.test_id)[Number of uses]
+            FROM Test JOIN Exam on Test.id = Exam.test_id
+            GROUP BY Test.name;*/
+
+            var data = from t in db.Tests
+                       join e in db.Exams
+                       on t.id equals e.test_id
+                       select t.name;
+
+            foreach (var v in data)
+            {
+                Console.WriteLine(v);
+            }
+
+            ViewBag.CurrentNameExam = nameExam;
+            ViewBag.CurrentDateStart = dateStart;
+            ViewBag.CurrentDateEnd = dateEnd;
+
+
             return View();
         }
     }
---
+++ OnlineAptitudeExam/Models/CustomModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineAptitudeExam.Models
+{
+    public class CustomModel
+    {
+        public class Custom {
+            public int id { get; set; }
+
+            public string nameExam { get; set; }
+
+            public int NumOFUses { get; set; }
+        }
+    }
+}
